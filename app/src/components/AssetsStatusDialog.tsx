61ce0ac48fd65bf5c9cbaef85d406a674ae9a5d9
Marcin <strong.neon@gmail.com>
2023-02-28 15:57:18 +0100

    AssetsStatusDialog - bar chart

--- app/src/components/AssetsStatusDialog.tsx
+++ app/src/components/AssetsStatusDialog.tsx
@@ -7,23 +7,81 @@ import {
   DialogContentText,
   DialogTitle,
 } from "@mui/material";
+import React, { useMemo } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { useTranslation } from "next-i18next";
+import useFormat from "@src/utils/useFormat";
+import { assetsPalette } from "@src/config/theme";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
 export default function AssetsStatusDialog(props: {
   open: boolean;
   onClose: () => void;
   assets?: AssetSnapshot[];
 }) {
+  const { t } = useTranslation();
+  const { amountFormat } = useFormat();
+  const otherGroupLabel = t("portfolioStatus.otherGroup");
+
+  const groupsData = useMemo(() => {
+    const data: Record<string, AssetSnapshot[]> = {};
+    props.assets?.forEach((asset) => {
+      const group = asset.group ?? otherGroupLabel;
+      if (!data[group]) {
+        data[group] = [];
+      }
+      data[group].push(asset);
+    });
+    return data;
+  }, [props.assets, t]);
+
+  const data: ChartData<"bar"> = useMemo(() => {
+    const labels = Object.keys(groupsData);
+    return {
+      labels,
+      datasets:
+        props.assets?.map(({ group, value, name }, index) => ({
+          label: name,
+          data: labels.map((label) =>
+            label === (group ?? otherGroupLabel) ? value ?? null : null
+          ),
+          backgroundColor: assetsPalette[index],
+        })) ?? [],
+    };
+  }, [props.assets, groupsData]);
+
+  const options: ChartOptions<"bar"> = useMemo(() => {
+    return {
+      indexAxis: "y",
+      responsive: true,
+      plugins: {
+        legend: { position: "bottom" },
+      },
+      scales: {
+        x: { stacked: true },
+        y: { stacked: true },
+      },
+    };
+  }, []);
+
   const handleClose = props.onClose;
   return (
     <Dialog open={props.open} onClose={handleClose}>
       <DialogTitle>Assets Status</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          {props.assets?.map((asset) => (
-            <div key={asset.id}>
-              {asset.name} - {asset.value}
-            </div>
-          ))}
+          <Bar data={data} options={options} />
         </DialogContentText>
       </DialogContent>
       <DialogActions>
