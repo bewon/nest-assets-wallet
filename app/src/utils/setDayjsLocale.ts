337fd8fe2432cd9d23e279f0588b1c4a121b6861
Marcin Kobiela <strong.neon@gmail.com>
2023-03-29 14:38:12 +0200

    History page

--- app/src/utils/setDayjsLocale.ts
+++ app/src/utils/setDayjsLocale.ts
@@ -1,7 +1,9 @@
 import dayjs from "dayjs";
 export default async function setDayjsLocale(localeCode: string) {
   try {
-    await import(`dayjs/locale/${localeCode}.js`);
+    if (localeCode !== "en") {
+      await import(`dayjs/locale/${localeCode}.js`);
+    }
     dayjs.locale(localeCode);
   } catch (error) {
     console.error(`Failed to load Day.js locale '${localeCode}':`, error);
