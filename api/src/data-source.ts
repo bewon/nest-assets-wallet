47fc920792ae4ee9eb312c02f3d1385b15ff831d
Marcin Kobiela <strong.neon@gmail.com>
2023-08-08 10:33:25 +0200

    TypeORM data-source prod ready

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -1,15 +1,16 @@
 import { DataSource } from 'typeorm';
 import { DataSourceOptions } from 'typeorm/data-source/DataSourceOptions';
+import 'dotenv/config';
 
 const dev = process.env.NODE_ENV === 'development';
 
 export const dataSourceConfig: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST ?? 'localhost',
   port: 5432,
-  username: 'assets_wallet',
-  password: 'awd9571',
-  database: 'assets_wallet',
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migration/*.[tj]s'],
   synchronize: false,
