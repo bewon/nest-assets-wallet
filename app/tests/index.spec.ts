84be742c04b7061e813279a9907427e895b4c93f
Marcin Kobiela <strong.neon@gmail.com>
2023-08-23 13:26:28 +0200

    index playwright tests

--- app/tests/index.spec.ts
+++ app/tests/index.spec.ts
@@ -11,3 +11,29 @@ test("has title", async ({ page }) => {
 test("has progress component with progressbar role", async ({ page }) => {
   await expect(page.getByRole("progressbar")).toBeVisible();
 });
+
+test("should be redirected to /auth/login if session is missing", async ({
+  page,
+}) => {
+  await page.evaluate(() => {
+    localStorage.removeItem("session-data");
+  });
+
+  await page.waitForTimeout(1000);
+
+  expect(page.url()).toBe("http://localhost:3000/auth/login");
+});
+test("should be redirected to /snapshot if session is missing", async ({
+  page,
+}) => {
+  await page.evaluate(() => {
+    localStorage.setItem(
+      "session-data",
+      JSON.stringify({ accessToken: "hbgujbngfu", userEmail: "test@bewon.eu" })
+    );
+  });
+
+  await page.waitForTimeout(1000);
+
+  expect(page.url()).toBe("http://localhost:3000/snapshot");
+});
