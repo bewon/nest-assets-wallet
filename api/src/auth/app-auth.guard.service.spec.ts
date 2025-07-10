4c2381b5873ba5dbcb0c96390d89e0084bd684f4
Marcin Kobiela <strong.neon@gmail.com>
2025-07-10 21:00:51 +0200

    Add unit tests for AppAuthGuard

--- api/src/auth/app-auth.guard.service.spec.ts
+++ api/src/auth/app-auth.guard.service.spec.ts
@@ -1,7 +1,60 @@
-import { AppAuthGuard } from './app-auth.guard.service';
+import { AppAuthGuard, SKIP_JWT_AUTH_KEY } from './app-auth.guard.service';
+import { ExecutionContext, SetMetadata } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 
-describe('JwtAuthGuard', () => {
+describe('AppAuthGuard', () => {
+    let guard: AppAuthGuard;
+
+    const mockExecutionContext = (
+        hasDecorator?: boolean,
+    ): ExecutionContext => {
+        const context: Partial<ExecutionContext> = {
+            getHandler: jest.fn(),
+            getClass: jest.fn(),
+        };
+        if (hasDecorator) {
+            SetMetadata(SKIP_JWT_AUTH_KEY, true)(context.getHandler as any);
+        }
+        return context as ExecutionContext;
+    };
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [AppAuthGuard],
+        }).compile();
+
+        guard = module.get<AppAuthGuard>(AppAuthGuard);
+    });
+
     it('should be defined', () => {
-        expect(new AppAuthGuard(null as any)).toBeDefined();
+        expect(guard).toBeDefined();
+    });
+
+    it('should call super.canActivate when SkipAppAuth decorator is not used', async () => {
+        const context = mockExecutionContext();
+        const superCanActivate = jest
+            .spyOn(Object.getPrototypeOf(guard), 'canActivate')
+            .mockResolvedValue(true);
+
+        await guard.canActivate(context);
+        expect(superCanActivate).toBeCalled();
+    });
+
+    it('should return true when super.canActivate returns true', async () => {
+        const context = mockExecutionContext();
+        jest
+            .spyOn(Object.getPrototypeOf(guard), 'canActivate')
+            .mockResolvedValue(true);
+        const canActivate = await guard.canActivate(context);
+        expect(canActivate).toBe(true);
+    });
+
+    it('should return false when super.canActivate returns false', async () => {
+        const context = mockExecutionContext();
+        jest
+            .spyOn(Object.getPrototypeOf(guard), 'canActivate')
+            .mockResolvedValue(false);
+        const canActivate = await guard.canActivate(context);
+        expect(canActivate).toBe(false);
     });
 }); 
